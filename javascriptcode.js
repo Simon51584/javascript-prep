32c33821b7906aeeadfd2c46546ce5b26ae5e863
Simon51584 <simon51584@gmail.com>
2020-11-16 15:39:07 -0600

    working on arrays practices

---
+++ javascriptcode.js
@@ -0,0 +1,14 @@
+let shoppingList = ["Butter", "Milk", "Eggs"];
+console.log(shoppingList)
+
+shoppingList = ["Eggs", "Butter", "Apples"];
+
+let eggs = shoppingList[0];
+shoppingList[1] = "Cheese"
+
+let listLength = shoppingList.length
+
+shoppingList.push("Butter");
+let butter = shoppingList.pop();
+
+
